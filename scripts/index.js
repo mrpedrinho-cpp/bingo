49c923a6dc58e623fd031bfd60c554e7a1eacb5f
Pedro <pedrohsa31@gmail.com>
2026-05-11 10:01:28 -0300

    adiciona configuração de bots e cartelas com botões de incremento e decremento e opção de tempo entre os números

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,44 @@
+//Botão de iniciar jogo
 const iniciarBtn = document.querySelector('.iniciar');
 
 iniciarBtn.addEventListener('click', () => {
     console.log('Iniciar jogo');
+});
+
+//Botões de mais e menos para os inputs de bots
+const inputMaisBots = document.querySelector('#mais-bots');
+const inputMenosBots = document.querySelector('#menos-bots');
+const inputBots = document.querySelector('#input-bots');
+
+inputMaisBots.addEventListener('click', () => {
+    let valor = parseInt(inputBots.value);
+    if (valor < 10) {
+        inputBots.value = valor + 1;
+    }
+});
+
+inputMenosBots.addEventListener('click', () => {
+    let valor = parseInt(inputBots.value);
+    if (valor > 1) {
+        inputBots.value = valor - 1;
+    }
+});
+
+//Botões de mais e menos para os inputs de cartelas
+const inputMaisCartelas = document.querySelector('#mais-cartelas');
+const inputMenosCartelas = document.querySelector('#menos-cartelas');
+const inputCartelas = document.querySelector('#input-cartelas');
+
+inputMaisCartelas.addEventListener('click', () => {
+    let valor = parseInt(inputCartelas.value);
+    if (valor < 6) {
+        inputCartelas.value = valor + 1;
+    }
+});
+
+inputMenosCartelas.addEventListener('click', () => {
+    let valor = parseInt(inputCartelas.value);
+    if (valor > 1) {
+        inputCartelas.value = valor - 1;
+    }
 });
